fdf66eff8242d7e600beb1bd40dd8db0ce86839f
krdg10 <krdg10@live.com>
2022-06-27 03:02:42 -0300

    placar oxo, legenda

--- lib/game.dart
+++ lib/game.dart
@@ -13,7 +13,11 @@ class _GameState extends State<Game> {
   String imagemEscolhaApp = 'circulo.png';
   String resultado = '';
   String escolhaApp = '';
+  int winApp = 0;
+  int winPlayer = 0;
   GameLogic gamelogic = GameLogic();
+  List imagensApp = ['circulo.png', 'x.png', 'circulo.png'];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,17 +26,65 @@ class _GameState extends State<Game> {
           child: Text('Jokenpo'),
         ),
       ),
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(
-                top: 8.0,
-                bottom: 16.0,
+      body: Padding(
+        padding: const EdgeInsets.only(top: 8.0),
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(
+                  top: 8.0,
+                  left: 55.0,
+                  right: 55.0,
+                ),
+                child: Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Você',
+                        style: TextStyle(
+                          color: Colors.blueAccent,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                      Text(
+                        'App',
+                        style: TextStyle(
+                          color: Colors.blueAccent,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ),
-              child: Center(
+              Container(
+                child: GridView.builder(
+                  shrinkWrap: true,
+                  //itemCount: 3,
+                  itemCount: imagensApp.length,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                  ),
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.only(
+                        left: 16,
+                        right: 16,
+                      ),
+                      child: Image.asset(
+                        imagensApp[index],
+                      ),
+                    );
+                  },
+                ),
+              ),
+              Container(
                 child: Text(
-                  'Escolha do App',
+                  resultado,
                   style: TextStyle(
                     color: Colors.blueAccent,
                     fontWeight: FontWeight.bold,
@@ -40,52 +92,65 @@ class _GameState extends State<Game> {
                   ),
                 ),
               ),
-            ),
-            Container(
-              child: Image.asset(
-                imagemEscolhaApp,
-                width: 250,
-                height: 250,
-              ),
-            ),
-            Container(
-              child: Text(
-                resultado,
-                style: TextStyle(
-                  color: Colors.blueAccent,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 24,
+              Padding(
+                padding: const EdgeInsets.only(top: 24.0),
+                child: Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      botao(context, 'tesoura'),
+                      botao(context, 'pedra'),
+                      botao(context, 'papel'),
+                    ],
+                  ),
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 24.0),
-              child: Container(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    botao(context, 'tesoura'),
-                    botao(context, 'pedra'),
-                    botao(context, 'papel'),
-                  ],
-                ),
-              ),
-            )
-          ],
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  Text Texto(String texto, int numeroDePontos) {
+    if (texto == 'Placar') {
+      return Text(
+        texto,
+        style: TextStyle(
+          color: Color.fromARGB(255, 42, 48, 58),
+          fontWeight: FontWeight.bold,
+          fontSize: 16,
+        ),
+      );
+    } else {
+      return Text(
+        texto + ' ' + numeroDePontos.toString(),
+        style: TextStyle(
+          color: Color.fromARGB(255, 42, 48, 58),
+          fontWeight: FontWeight.bold,
+          fontSize: 16,
+        ),
+      );
+    }
   }
 
   GestureDetector botao(BuildContext context, String jogada) {
     return GestureDetector(
       onTap: () {
         modal(context);
-        setState(() {
-          escolhaApp = gamelogic.takeRandomValue(widget.level, jogada);
-          imagemEscolhaApp = escolhaApp + '.png';
-          resultado = gamelogic.selectWinner(jogada, escolhaApp);
-        });
+        setState(
+          () {
+            escolhaApp = gamelogic.takeRandomValue(widget.level, jogada);
+            imagensApp[2] = escolhaApp + '.png';
+            imagensApp[0] = jogada + '.png';
+            resultado = gamelogic.selectWinner(jogada, escolhaApp);
+            if (resultado == "Você venceu!") {
+              winPlayer = winPlayer + 1;
+            } else if (resultado == "Você perdeu") {
+              winApp = winApp + 1;
+            }
+          },
+        );
       },
       child: Image.asset(
         jogada + '.png',
